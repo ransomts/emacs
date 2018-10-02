b31101dd8748b13ac41a98f174cd8b0db50f9148
Tim Ransom <ransomtim8078@gmail.com>
2018-10-02 19:54:37 -0400

    messing with some erc stuff
    turns out twitch chat can run through irc, so I want that
    
    Also use loopback for pinentry, the gui is garbage

--- lisp/customize.el
+++ lisp/customize.el
@@ -6,6 +6,7 @@
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(epa-pinentry-mode (quote loopback))
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
