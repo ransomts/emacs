14d52c0ddd05f1b99359dc5e5957e766efb69246
Tim Ransom <tsranso@g.clemson.edu>
2019-01-23 12:07:03 -0500

    update to packages

---
+++ elpa/transmission-20190107.408/transmission-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "transmission" "20190107.408" "Interface to a Transmission session" '((emacs "24.4") (let-alist "1.0.5")) :commit "4aae3a4b56f60a416002d7ce4282bd44e0178b54" :keywords '("comm" "tools") :authors '(("Mark Oteiza" . "mvoteiza@udel.edu")) :maintainer '("Mark Oteiza" . "mvoteiza@udel.edu"))
