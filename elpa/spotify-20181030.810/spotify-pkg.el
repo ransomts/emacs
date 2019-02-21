d3419980171460e9e699c360f47a2225c96bd172
Tim Ransom <ransomtim8078@gmail.com>
2019-02-21 14:42:16 -0500

    add spotify integration

---
+++ elpa/spotify-20181030.810/spotify-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "spotify" "20181030.810" "Control the spotify application from emacs" '((cl-lib "0.5")) :commit "29577cf1188161f98b8358c149aaf47b2c137902" :keywords '("convenience") :authors '(("R.W. van 't Veer")) :maintainer '("R.W. van 't Veer") :url "https://github.com/remvee/spotify-el")
