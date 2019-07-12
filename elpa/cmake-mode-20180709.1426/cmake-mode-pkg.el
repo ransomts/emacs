705dbb41c0e0509e391182a66bf66f8523f4ee05
ransomts <ransomtim8078@gmail.com>
2019-07-11 21:16:19 -0400

    package update

--- elpa/cmake-mode-20180709.1426/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20180709.1426" "major-mode for editing CMake sources" 'nil :commit "0bddcce987d15b92f7f98a5bddcb74a8bc0251d3")
---
+++ elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "09f9ed600954bba2cd51529471554f49ec9e855e")
