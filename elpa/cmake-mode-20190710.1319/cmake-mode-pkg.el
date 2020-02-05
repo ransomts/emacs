549a28261f2c5467d66bdc65af17197e13683f83
Tim Ransom <tsranso@g.clemson.edu>
2020-02-04 21:57:04 -0500

    ready to purge elpa directory

--- elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
+++ elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "feea34e7eb483e8db28947920757612a95ab1863")
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "304703fb473fd0e4aa2627585907c4ea999beb58")
