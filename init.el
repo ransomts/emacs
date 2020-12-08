4f09da429fbe7deff607c3909c5ad2419cbecfab
Tim Ransom <ransomtim8078@gmail.com>
2020-12-08 18:03:47 -0500

    gcal update
    fixed the material theme line extension too

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+(setq package-archives '( ("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
